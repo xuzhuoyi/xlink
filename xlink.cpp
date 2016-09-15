4705dc00b749b2c5447edfe71b401a300521a62b
xuzhuoyi <xzy476386434@vip.qq.com>
2016-09-15 21:11:20 +0800

    Fixed compile error

--- xlink.cpp
+++ xlink.cpp
@@ -20,7 +20,7 @@ void Xlink::on_pButtonOpen_clicked()
 {
     m_fileName = QFileDialog::getOpenFileName(this, tr("打开文件"), "/", tr("任何支持的文件(*.bin *.hex);;所有文件(*.*)"));
     if (m_fileName.length() != 0)
-        ui->labelStatus->setText(tr("打开文件:") + fileName);
+        ui->labelStatus->setText(tr("打开文件:") + m_fileName);
 }
 
 void Xlink::on_pButtonConnect_clicked()
@@ -28,7 +28,7 @@ void Xlink::on_pButtonConnect_clicked()
     QString program = "opnocd";
     QStringList arguments;
     arguments << "-f" << m_configFileName;
-    QProcess *process = new QProcess(parent);
+    QProcess *process = new QProcess();
     process->setArguments(arguments);
     process->start(program, arguments);
 }
@@ -37,5 +37,5 @@ void Xlink::on_pButtonSetup_clicked()
 {
     m_configFileName = QFileDialog::getOpenFileName(this, tr("打开设备配置文件"), "/", tr("配置文件(*.cfg)"));
     if (m_configFileName.length() != 0)
-        ui->labelStatus->setText(tr("已打开配置文件文件:") + fileName);
+        ui->labelStatus->setText(tr("已打开配置文件文件:") + m_fileName);
 }
